e4ad2b8b9da99790e39a000d9fe7be5cfcdf37b7
hina <matsunohinako@matsunoakonombp.lan>
2023-02-16 21:22:29 +0900

    Fix background color

--- pokemon/src/App.tsx
+++ pokemon/src/App.tsx
@@ -3,19 +3,17 @@ import { css } from '@emotion/react'
 
 import { PokemonList } from "./components/PokemonList"
 import { Search } from "./components/Search"
-import { SearchWordProvider } from "./providers/SerchWordProvider"
 
 const style = css({
-  background: 'linear-gradient( to bottom,#dad4ec 0%,#f3e7e9 100% )',
+  // background: 'linear-gradient( to bottom,#dad4ec 0%,#f3e7e9 100% )',
+  background: "transparent"
 })
 
 function App() {
   return (
     <div css={[style]}>
-      <SearchWordProvider>
-        <Search />
-        <PokemonList />
-      </SearchWordProvider>
+      <Search></Search>
+      <PokemonList ></PokemonList >
     </div>
   )
 }
