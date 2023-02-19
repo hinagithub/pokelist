00b661cab16fe9b0ce202a1e116fd225a00d58ad
hinagithub <matsuno.hinako@gmail.com>
2023-02-19 18:46:23 +0900

    Apply prettier

--- src/api/pokemon.ts
+++ src/api/pokemon.ts
@@ -1,50 +1,47 @@
-import axios from "axios";
+import axios from 'axios'
 
 const POKEMON_URL = 'https://pokeapi.co/api/v2/pokemon'
 const SPECIES_URL = 'https://pokeapi.co/api/v2/pokemon-species'
-const TYPE_URL = "https://pokeapi.co/api/v2/type"
+const TYPE_URL = 'https://pokeapi.co/api/v2/type'
 
 export const api = {
+  /**
+   * ポケモン一覧取得
+   */
+  getPokemonSumarries: async (limit: number): Promise<any> => {
+    const res = await axios.get(`${POKEMON_URL}?limit=${limit}`)
+    return res.data
+  },
 
-    /**
-     * ポケモン一覧取得
-     */
-    getPokemonSumarries: async (limit: number): Promise<any> => {
-        const res = await axios.get(`${POKEMON_URL}?limit=${limit}`)
-        return res.data
-    },
+  /**
+   * ポケモン詳細取得
+   */
+  getPokemonDetail: async (pokemonName: string): Promise<any> => {
+    const res = await axios.get(`${POKEMON_URL}/${pokemonName}`)
+    return res.data
+  },
 
-    /**
-     * ポケモン詳細取得
-     */
-    getPokemonDetail: async (pokemonName: string): Promise<any> => {
-        const res = await axios.get(
-            `${POKEMON_URL}/${pokemonName}`
-        )
-        return res.data
-    },
+  /**
+   * ポケモン種族設定取得 (英語名->日本語名の翻訳に使用する)
+   */
+  getSpecies: async (id: number | string): Promise<any> => {
+    const res = await axios.get(`${SPECIES_URL}/${id}`)
+    return res.data
+  },
 
-    /**
-     * ポケモン種族設定取得 (英語名->日本語名の翻訳に使用する)
-     */
-    getSpecies: async (id: number | string): Promise<any> => {
-        const res = await axios.get(`${SPECIES_URL}/${id}`)
-        return res.data
-    },
+  /**
+   * タイプ一覧取得
+   */
+  getTypeSummaries: async (): Promise<any> => {
+    const res = await axios.get(TYPE_URL)
+    return res.data
+  },
 
-    /**
-     * タイプ一覧取得
-     */
-    getTypeSummaries: async (): Promise<any> => {
-        const res = await axios.get(TYPE_URL)
-        return res.data
-    },
-
-    /**
-     * タイプの詳細を取得　(英語名->日本語名の翻訳に使用する)
-     */
-    getTypeDetail: async (name: string): Promise<any> => {
-        const res = await axios.get(`${TYPE_URL}/${name}`)
-        return res.data
-    }
+  /**
+   * タイプの詳細を取得　(英語名->日本語名の翻訳に使用する)
+   */
+  getTypeDetail: async (name: string): Promise<any> => {
+    const res = await axios.get(`${TYPE_URL}/${name}`)
+    return res.data
+  },
 }
--- src/types/pokemon.ts
+++ src/types/pokemon.ts
@@ -1,20 +1,19 @@
 export type Pokemon = {
-    id: number
-    name: string
-    url: string
-    types: string[]
+  id: number
+  name: string
+  url: string
+  types: string[]
 }
 
-
 export type PokeAPIType = {
-    slot: number
-    type: {
-        name: string
-        url: string
-    }
+  slot: number
+  type: {
+    name: string
+    url: string
+  }
 }
 
 export type TypeName = {
-    en: string
-    ja: string
+  en: string
+  ja: string
 }
